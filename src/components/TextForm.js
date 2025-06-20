f6058dcd80e9bb0c3f06a7df5b73339d111e7345
Dhruvin Lad <laddhruvin84@gmail.com>
2025-06-20 12:44:16 -0400

    Video 9 completed added voice function

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,6 +11,13 @@ export default function TextForm(props) {
     const handellowClick=()=>{
         //console.log("Uppercase was clicked");
         let newText = text.toLowerCase();
+        //let newText = text.toLocaleLowerCase();
+        setText(newText);
+    }
+
+    const handelClearClick=()=>{
+        //console.log("Uppercase was clicked");
+        let newText = '';
         setText(newText);
     }
 
@@ -18,6 +25,22 @@ export default function TextForm(props) {
         //console.log("On change");
         setText(event.target.value);
     }
+
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance(text);
+        window.speechSynthesis.speak(msg);
+        const toogle = document.getElementById('toggle')
+        if (toogle.textContent === "Speak") {
+            toogle.innerHTML = "Stop"
+        }
+        else {
+            toogle.innerHTML = "Speak"
+            if (toogle.innerHTML === "Speak"){
+                window.speechSynthesis.cancel()
+            }
+        }
+    }
+
 
     const [text , setText ] = useState("Enter Text here") ;
     return (
@@ -29,6 +52,8 @@ export default function TextForm(props) {
         </div>
         <button className="btn btn-primary mx-2" onClick={handelUpClick}>Convert to Uppercase </button>
         <button className="btn btn-primary" onClick={handellowClick}>Convert to lowercase </button>
+        <button className="btn btn-primary mx-2" onClick={handelClearClick}>Clear Text</button>
+        <button className="btn btn-primary" onClick={speak} id='toggle'> Speak </button>
     </div>
     <div className="container my-3">
         <h1>Your Text Summary</h1>
